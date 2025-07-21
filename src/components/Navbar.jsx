2d0d26f879ba686cd1d07749535c39623cb430e1
supun456 <pakaya*351>
2025-07-21 19:36:49 +0530

    new changes supun

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,8 +63,8 @@ const Navbar = () => {
           >
             <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
             <p className="text-white text-[20px] font-bold cursor-pointer flex">
-              LOHIT&nbsp;
-              <span className="sm:block hidden">KOLLURI</span>
+              SUPUN&nbsp;
+              <span className="sm:block hidden">PERERA</span>
             </p>
           </Link>
           {renderNavLinks(false)}
